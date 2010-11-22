bfb57f8a7ff63e11c9439014e2a0cd0bd3248dd2
Micah Walter <micahwalter@me.com>
2010-11-22 16:53:16 -0500

    adding drupal 7 core code as new branch

--- modules/forum/forum-icon.tpl.php
+++ modules/forum/forum-icon.tpl.php
@@ -1,9 +1,9 @@
 <?php
-// $Id: forum-icon.tpl.php,v 1.3 2007/12/20 09:35:09 goba Exp $
+// $Id: forum-icon.tpl.php,v 1.7 2010/10/03 00:41:14 dries Exp $
 
 /**
- * @file forum-icon.tpl.php
- * Display an appropriate icon for a forum post.
+ * @file
+ * Default theme implementation to display an appropriate icon for a forum post.
  *
  * Available variables:
  * - $new_posts: Indicates whether or not the topic contains new posts.
@@ -14,12 +14,14 @@
  * @see theme_forum_icon()
  */
 ?>
+<div class="topic-status-<?php print $icon_class ?>" title="<?php print $icon_title ?>">
 <?php if ($new_posts): ?>
-  <a name="new">
+  <a id="new">
 <?php endif; ?>
 
-<?php print theme('image', "misc/forum-$icon.png") ?>
+  <span class="element-invisible"><?php print $icon_title ?></span>
 
 <?php if ($new_posts): ?>
   </a>
 <?php endif; ?>
+</div>
